3759c0eccee0ab28822585f4e11743a3000706c8
smannas98 <seanmannas02@gmail.com>
2018-04-18 18:35:56 -0400

    fixed typo error

--- bloc-jams-react/src/components/Landing.js
+++ bloc-jams-react/src/components/Landing.js
@@ -17,7 +17,7 @@ const Landing = () => (
       <div className="point">
         <h2 className="point-title">Mobile enabled</h2>
         <p className="point-description">Listen to your music on the go.
-          This streaming service is available on all mobile platforms.</p>
+          this streaming service is available on all mobile platforms.</p>
       </div>
 
     </section>
